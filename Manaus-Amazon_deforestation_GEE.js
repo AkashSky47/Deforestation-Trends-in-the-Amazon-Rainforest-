7a56df57f926aedfe40fc7d8b2089a662a977e3a
AkashSky47 <141479991+AkashSky47@users.noreply.github.com>
2024-08-31 23:46:35 +0530

    Add files via upload

---
+++ Manaus-Amazon_deforestation_GEE.js
@@ -0,0 +1,36 @@
+
+// Load the Hansen Global Forest Change dataset as an Image
+var dataset = ee.Image('UMD/hansen/global_forest_change_2023_v1_11');
+
+// Select the forest loss year band
+var forestLossYear = dataset.select('lossyear');
+
+// Define the coordinates for your new Area of Interest (AOI)
+var aoi = ee.Geometry.Polygon([
+  [
+    [-60.18796207235581, -3.2557682921982174],
+    [-59.82815982626206, -3.2557682921982174],
+    [-59.82815982626206, -2.9211724598231332],
+    [-60.18796207235581, -2.9211724598231332],
+    [-60.18796207235581, -3.2557682921982174]  // Close the polygon by repeating Point 1
+  ]
+]);
+
+// Center the map on the new AOI
+Map.centerObject(aoi, 6);  // Adjust zoom level as necessary
+Map.addLayer(aoi, {color: 'blue'}, 'New Area of Interest');
+
+// Clip the forest loss year data to your new AOI
+var lossYearInNewAOI = forestLossYear.clip(aoi);
+
+// Display the clipped forest loss data on the map
+Map.addLayer(lossYearInNewAOI, {min: 0, max: 23, palette: ['white', 'red']}, 'Forest Loss Year in New AOI');
+
+// Export the forest loss year data for the new AOI to Google Drive as a GeoTIFF
+Export.image.toDrive({
+  image: lossYearInNewAOI,
+  description: 'ForestLossYear_NewAOI',
+  scale: 30,  // 30 meters resolution
+  region: aoi,
+  fileFormat: 'GeoTIFF'
+});
